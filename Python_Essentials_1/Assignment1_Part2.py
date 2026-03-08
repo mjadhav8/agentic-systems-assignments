da081e77b16740a2aa44224ed596f2f2828e2864
mjadhav8 <milindjadhav2020@gmail.com>
2026-03-08 22:34:50 +0000

    Update Assignment1_Part2.py

--- Python_Essentials_1/Assignment1_Part2.py
+++ Python_Essentials_1/Assignment1_Part2.py
@@ -1,1 +1,12 @@
-
+First_Name = input("Enter First Name")
+Last_Name = input("Enter Last Name")
+Age = input("Enter Age: ")
+try:
+    Age_Int = int(Age)
+    if Age_Int < 0:
+        print("Age cant be negative")
+    else:
+        print("Full Name: ", First_Name + Last_Name)
+        print("You will be " + str(Age_Int) + " next year")
+except:
+    print("Invalid Age Input")
